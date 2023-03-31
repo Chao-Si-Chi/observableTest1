677b3f41d8f9fd67386492b6fc7c979e6ec50148
Chao-Si-Chi <128582972+Chao-Si-Chi@users.noreply.github.com>
2023-03-31 16:04:26 +0800

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, VERSION, OnInit } from '@angular/core';
-import { filter, first, fromEvent, map, Observable, scan, take, interval, merge, zip, of } from 'rxjs';
+import { filter, first, fromEvent, map, Observable, scan, take, interval, merge, zip, of, race, forkJoin, retry, tap } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
 @Component({
@@ -33,10 +33,17 @@ export class AppComponent implements OnInit {
     //this.filteringTake();
     //this.filterTakeWithInterval();
 
-    this.combineMerge();
+    //this.combineMerge();
     //this.combineZip();
     //this.combineRace();
     //this.combineForkJoin();
+    
+    //this.othersRetry();
+    //this.othersTap();
+
+    //this.demoPromise();
+    //this.demoPromiseRace();
+    this.demoPromiseAll();
   }
 
   firstObservable(){
@@ -169,20 +176,120 @@ export class AppComponent implements OnInit {
   }
 
   combineZip(){
-    let age = of<number>(27, 25, 29, 33, 45);
+    /*let age = of<number>(27, 25, 29, 33, 45);
     let name = of<string>('Foo', 'Bar', 'Beer', 'Da');
     let isDev = of<boolean>(true, true, false);
     
     zip(age, name, isDev).pipe(
       map(([age, name, isDev]) => ({ age, name, isDev })),
-    ).subscribe(x => console.log(x));
+    ).subscribe(x => console.log(x));*/
   }
 
   combineRace(){
+    let timer1 = interval(1000).pipe(take(10), map( x=> 'timer1: ' + String(x)));
+    let timer2 = interval(2000).pipe(take(6), map( x=> 'timer2: ' + String(x)));
     
+    race(timer1, timer2).subscribe(x => console.log(`output: ${x}`));
   }
 
   combineForkJoin(){
-    
+    forkJoin({
+      foo: of(1, 2, 3, 4),
+      bar: Promise.resolve(8),
+      baz: interval(3000).pipe(take(2))
+    }).subscribe({
+      next: value => console.log(`forkJoin: ${JSON.stringify(value)}`),
+      complete: () => console.log("complete!!")
+    });
+  }
+
+  othersRetry(){
+    // throw error 
+    of('a', 'b', 'c', 2).pipe(
+      map( (v:string) =>  v.toUpperCase()),
+      //retry 2 times on error 
+      retry(2)
+    ).subscribe( 
+        v => console.log(`first: ${v}..`),
+        err => console.log(`first: Opps! There are some error: ${err}`),
+        () => console.log("first: complete!")
+      );
+
+    // run complete
+    of('a', 'b', 'c' ).pipe(
+        map( (v:string) =>  v.toUpperCase()),
+        //retry 2 times on error 
+        retry(2)
+      ).subscribe( 
+          v => console.log(`second: ${v}..`),
+          err => console.log(`second: Opps! There are some error: ${err}`),
+          () => console.log("second: complete!")
+        );
+  }
+
+  othersTap(){
+    // record value in console.log
+    of('a', 'b', 'c', 2).pipe(
+      tap( v => console.log(`before: ${v}`)),
+      map( (v:string) =>  v.toUpperCase()),
+      tap( v => console.log(`after: ${v}`)),
+    ).subscribe( 
+        v => console.log(`${v}..`),
+        err => console.log(`Opps! There are some error: ${err}`),
+        () => console.log("complete!")
+    );
+
+    // modify value in tap
+    of('a', 'b', 'c', 2).pipe(
+      tap( v => {
+        return 'x';
+      }),
+      map( (v:string) =>  v.toUpperCase()),
+      tap( v => console.log(`modfiy: after: ${v}`)),
+    ).subscribe( 
+        v => console.log(`modify: ${v}..`),
+        err => console.log(`modify: Opps! There are some error: ${err}`),
+        () => console.log("modify: complete!")
+    );
+  }
+
+  demoPromise(){
+    let declaration = new Promise((resolve, reject) => {
+      console.log('hello in Promise declaration')
+      resolve('world');
+    });
+
+    setTimeout(
+        ()=>declaration.then((v)=>console.log(`say: ${v} in then statement.`))
+        , 5000
+    );
+  }
+
+  demoPromiseRace(){
+    var p1 = new Promise(function(resolve, reject) { 
+      setTimeout(() => resolve('one') , 500)
+    });
+    var p2 = new Promise(function(resolve, reject) { 
+      setTimeout(() => resolve('two') , 100); 
+    });
+  
+    Promise.race([p1, p2]).then(function(value) {
+      console.log(value); 
+      // "two"
+    });
+  }
+
+  demoPromiseAll(){
+    var p1 = new Promise(function(resolve, reject) { 
+      setTimeout(() => resolve('one') , 5000)
+    });
+    var p2 = new Promise(function(resolve, reject) { 
+      setTimeout(() => resolve('two') , 1000); 
+    });
+
+    Promise.all([p1, p2]).then(function(value) {
+      console.log(value); 
+      // ['one', 'two']
+    });
   }
 }
