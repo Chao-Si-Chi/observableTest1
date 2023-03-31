6870740032285780925369df445ee0f92d9bb559
Chao-Si-Chi <128582972+Chao-Si-Chi@users.noreply.github.com>
2023-03-31 15:22:30 +0800

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, VERSION, OnInit } from '@angular/core';
-import { filter, fromEvent, map, Observable, of, scan } from 'rxjs';
+import { filter, first, fromEvent, map, Observable, scan, take, interval, merge, zip, of } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
 @Component({
@@ -28,7 +28,15 @@ export class AppComponent implements OnInit {
 
     //this.demoPipe();
 
-    this.filteringFilter();
+    //this.filteringFilter();
+    //this.filteringFirst();
+    //this.filteringTake();
+    //this.filterTakeWithInterval();
+
+    this.combineMerge();
+    //this.combineZip();
+    //this.combineRace();
+    //this.combineForkJoin();
   }
 
   firstObservable(){
@@ -120,5 +128,61 @@ export class AppComponent implements OnInit {
     of('Axxxxxx2', 'O126666664','A123456789').pipe(
       filter(x => (/^[A-Z]\d{8}[^1,2,6]/g).test(x))
     ).subscribe(v => console.log(`pass regex: ${v}`));
+  }
+
+  filteringFirst(){
+    of('Axxxxxx2', 'O126666664','A123456781').pipe(
+      first()
+    ).subscribe(
+        v => console.log(`first value: ${v}`), 
+        err => console.log(`Oops! there are some error!`) ,
+        () => console.log('complete')
+    );
+  }
+
+  filteringTake(){
+    of(1,2,3,4,5,6,7,8).pipe(
+      take(2)
+    ).subscribe(
+        v => console.log(`value: ${v}`), 
+        err => console.log(`Oops! there are some error!`) ,
+        () => console.log('complete')
+    );
+  }
+
+  filterTakeWithInterval(){
+    console.log('我數到 3');
+    interval(1).pipe(
+      map( x => x + 1),
+      take(3)
+    ).subscribe(
+      v => console.log(`${v}..`), 
+      err => console.log(`Oops! there are some error!`) ,
+      () => console.log('好!回家!')
+    );
+  }
+
+  combineMerge(){
+    let timer1 = interval(1000).pipe(take(10), map( x=> 'timer1: ' + String(x)));
+    let timer2 = interval(2000).pipe(take(6), map( x=> 'timer2: ' + String(x)));
+    merge(timer1, timer2).subscribe(x => console.log(`output: ${x}`));
+  }
+
+  combineZip(){
+    let age = of<number>(27, 25, 29, 33, 45);
+    let name = of<string>('Foo', 'Bar', 'Beer', 'Da');
+    let isDev = of<boolean>(true, true, false);
+    
+    zip(age, name, isDev).pipe(
+      map(([age, name, isDev]) => ({ age, name, isDev })),
+    ).subscribe(x => console.log(x));
+  }
+
+  combineRace(){
+    
+  }
+
+  combineForkJoin(){
+    
   }
 }
